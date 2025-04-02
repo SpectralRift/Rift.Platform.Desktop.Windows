04288cca7f527f4c3e681d698117a914d1f60e06
Alexandru Talnaci <flafyrex@gmail.com>
2025-04-02 16:18:28 +0300

    Platform update for Windows subsystem
    Added platform code for Windows subsystem.

--- private/Engine/Platform/Windows/Win32_EngineMain.cpp
+++ private/Engine/Platform/Windows/Win32_EngineMain.cpp
@@ -1,8 +1,18 @@
+#include <Windows.h>
+
 // reference to EngineMain
 extern int EngineMain(int argc, char *argv[]);
 
 void EnginePreShutdown() {}
 
+#ifdef USE_WINDOWED_SUBSYSTEM
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    return EngineMain(0, nullptr);
+}
+#else
+
 int main(int argc, char *argv[]) {
     return EngineMain(argc, argv);
 }
+
+#endif
